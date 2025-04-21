a4e9e67b4147039b7b489bd99d355f6699273eb3
99-kofi <98652285+99-kofi@users.noreply.github.com>
2025-04-21 23:49:09 +0000

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig, loadEnv } from 'vite';
+import path from 'path';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env': {
+        API_KEY: JSON.stringify(env.VITE_API_KEY),
+      }
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      }
+    }
+  };
+});
